05784c39bc27605d9df2cf3299e8644d931e0344
Oyukawa <kykw.6.swimming@gmail.com>
2023-12-16 09:52:17 +0900

    ✨ init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
@@ -15,7 +15,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
+            By{" "}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
@@ -29,14 +29,7 @@ export default function Home() {
       </div>
 
       <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+        <h1>Flexbox PlayGround</h1>
       </div>
 
       <div className={styles.grid}>
@@ -91,5 +84,5 @@ export default function Home() {
         </a>
       </div>
     </main>
-  )
+  );
 }
